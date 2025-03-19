55f74ac3364a687559c655a16500e8e2dc4c9c73
Daniel Zaharia <Daniel.Zaharia@centric.eu>
2025-03-19 14:48:38 +0200

    added CourseClassLibrary and Session1 projects

---
+++ CourseClassLibrary/PageCount.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseClassLibrary
+{
+    public struct PageCount
+    {
+        public int Value { get; }
+
+        public PageCount(int value)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => $"{Value} pages";
+    }
+}
---
+++ Session1/MemoryTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit.Abstractions;
+
+namespace Session1
+{
+    public class MemoryTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MemoryTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Setup for test started.");
+        }
+    }
+}
