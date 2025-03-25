b9d6a07fb430885cb82773cadae51960eb266208
Daniel Zaharia <Daniel.Zaharia@centric.eu>
2025-03-25 16:00:22 +0200

    fixes

--- Session1/BookTests.cs
+++ Session1/BookTests.cs
@@ -14,32 +14,45 @@ namespace Session1
         }
 
         [Fact]
-        public void Book_CanBeInstantieted()
+        public void Book_CanBeInstantiated()
         {
             var book = new Book("The Book", "The Author");
 
-            _output.WriteLine(book.Title);
-            _output.WriteLine(book.Author);
+            _output.WriteLine($"Book title => {book.Title}");
+            _output.WriteLine($"Book author => {book.Author}");
 
             Assert.Equal("The Book", book.Title);
             Assert.Equal("The Author", book.Author);
         }
 
         [Fact]
-        public void GetBookInfo()
+        public void GetBookInfo_ReturnsCorrectFormat()
         {
             var book = new Book("The Book", "The Author");
 
-            _output.WriteLine(book.GetBookInfo());
+            _output.WriteLine($"Book info => {book.GetBookInfo()}");
         }
 
         [Fact]
-        public void UniqueId_IsGeneratedForEachBookObject()
+        public void HasManyPages_ReturnsTrue_WhenBookMoreThanMaxPages()
+        {
+            var book = new Book("War and Peace", "Leo Tolstoy");
+            var nrPages = 1001;
+
+            if (book.HasManyPages(nrPages))
+                _output.WriteLine("Book has many pages");
+            else
+                _output.WriteLine("Book does not have many pages");
+        }
+
+        [Fact]
+        public void UniqueId_IsDifferentForEachInstance()
         {
             var book1 = new Book("The Book", "The Author");
             var book2 = new Book("The Book", "The Author");
-            _output.WriteLine(book1.UniqueId);
-            _output.WriteLine(book2.UniqueId);
+
+            _output.WriteLine($"Book1 id => {book1.UniqueId}");
+            _output.WriteLine($"Book2 id => {book2.UniqueId}");
 
             Assert.NotEqual(book1.UniqueId, book2.UniqueId);
         }
--- Session1/MemoryTests.cs
+++ Session1/MemoryTests.cs
@@ -19,28 +19,28 @@ namespace Session1
             var book1 = new Book("The Book", "The Author");
             var book2 = book1;
 
-            _output.WriteLine(book1.GetBookInfo());
-            _output.WriteLine(book2.GetBookInfo());
+            _output.WriteLine($"Book1 info => {book1.GetBookInfo()}");
+            _output.WriteLine($"Book2 info => {book2.GetBookInfo()}");
 
             book2.Title = "The New Book";
 
-            _output.WriteLine(book1.GetBookInfo());
-            _output.WriteLine(book2.GetBookInfo());
+            _output.WriteLine($"Book1 info => {book1.GetBookInfo()}");
+            _output.WriteLine($"Book2 info => {book2.GetBookInfo()}");
         }
 
         [Fact]
         public void ValueType_DoesNotChangeOriginal()
         {
-            var pageCount1 = new PageCount(100);
-            var pageCount2 = pageCount1;
+            var pages1 = new PageCount(200);
+            var pages2 = pages1;
 
-            _output.WriteLine(pageCount1.GetNumberOfPages());
-            _output.WriteLine(pageCount2.GetNumberOfPages());
+            _output.WriteLine($"Pages1 => {pages1.GetNumberOfPages()}");
+            _output.WriteLine($"Pages2 => {pages2.GetNumberOfPages()}");
 
-            pageCount2 = new PageCount(200);
+            pages2 = new PageCount(500);
 
-            _output.WriteLine(pageCount1.GetNumberOfPages());
-            _output.WriteLine(pageCount2.GetNumberOfPages());
+            _output.WriteLine($"Pages1 => {pages1.GetNumberOfPages()}");
+            _output.WriteLine($"Pages2 => {pages2.GetNumberOfPages()}");
         }
     }
 }
