dcd829c436ecce49a4048b319bfa326092013b8d
Daniel Zaharia <Daniel.Zaharia@centric.eu>
2025-03-25 15:56:19 +0200

    added session-1 demo

--- Session1/BookTests.cs
+++ Session1/BookTests.cs
@@ -1,3 +1,4 @@
+using CourseClassLibrary;
 using Xunit.Abstractions;
 
 namespace Session1
@@ -12,5 +13,35 @@ namespace Session1
             _output.WriteLine("Setup for test started.");
         }
 
+        [Fact]
+        public void Book_CanBeInstantieted()
+        {
+            var book = new Book("The Book", "The Author");
+
+            _output.WriteLine(book.Title);
+            _output.WriteLine(book.Author);
+
+            Assert.Equal("The Book", book.Title);
+            Assert.Equal("The Author", book.Author);
+        }
+
+        [Fact]
+        public void GetBookInfo()
+        {
+            var book = new Book("The Book", "The Author");
+
+            _output.WriteLine(book.GetBookInfo());
+        }
+
+        [Fact]
+        public void UniqueId_IsGeneratedForEachBookObject()
+        {
+            var book1 = new Book("The Book", "The Author");
+            var book2 = new Book("The Book", "The Author");
+            _output.WriteLine(book1.UniqueId);
+            _output.WriteLine(book2.UniqueId);
+
+            Assert.NotEqual(book1.UniqueId, book2.UniqueId);
+        }
     }
 }
--- Session1/MemoryTests.cs
+++ Session1/MemoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit.Abstractions;
+﻿using CourseClassLibrary;
+using Xunit.Abstractions;
 
 namespace Session1
 {
@@ -10,6 +11,36 @@ namespace Session1
         {
             _output = output;
             _output.WriteLine("Setup for test started.");
+        }
+
+        [Fact]
+        public void ReferenceType_ChangesOriginalObject()
+        {
+            var book1 = new Book("The Book", "The Author");
+            var book2 = book1;
+
+            _output.WriteLine(book1.GetBookInfo());
+            _output.WriteLine(book2.GetBookInfo());
+
+            book2.Title = "The New Book";
+
+            _output.WriteLine(book1.GetBookInfo());
+            _output.WriteLine(book2.GetBookInfo());
+        }
+
+        [Fact]
+        public void ValueType_DoesNotChangeOriginal()
+        {
+            var pageCount1 = new PageCount(100);
+            var pageCount2 = pageCount1;
+
+            _output.WriteLine(pageCount1.GetNumberOfPages());
+            _output.WriteLine(pageCount2.GetNumberOfPages());
+
+            pageCount2 = new PageCount(200);
+
+            _output.WriteLine(pageCount1.GetNumberOfPages());
+            _output.WriteLine(pageCount2.GetNumberOfPages());
         }
     }
 }
