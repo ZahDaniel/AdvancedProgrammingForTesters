336c31a272a3d130c6c5028d970ca2801111c0f9
Andrada Bogdan <andrada.bogdan@centric.eu>
2025-03-27 16:44:15 +0200

    added interface

---
+++ CourseClassLibrary/LibraryItem.cs
@@ -0,0 +1,23 @@
+﻿namespace CourseClassLibrary
+{
+    public abstract class LibraryItem
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string UniqueId { get; } = Guid.NewGuid().ToString();
+
+        protected LibraryItem(string title, string author)
+        {
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author))
+            {
+                throw new ArgumentException("Title and Author cannot be empty.");
+            }
+
+            Title = title;
+            Author = author;
+        }
+
+        public abstract string GetItemInfo();
+    }
+
+}
