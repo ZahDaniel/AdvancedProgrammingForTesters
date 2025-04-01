f0b7346902f4fa799897e7791b2c033678fc35b2
Cristina Murariu <Cristina.Murariu@centric.eu>
2025-04-01 16:30:04 +0300

    Added homework for Session 1

---
+++ CourseClassLibrary/Domain/Podcasts/Podcast.cs
@@ -0,0 +1,29 @@
+﻿namespace CourseClassLibrary.Domain.Podcasts
+{
+    public class Podcast
+    {
+        public string Name { get; set; }
+
+        public string Host { get; set; }
+
+        public int LaunchYear { get; }
+
+        private const int EpisodesLimit = 20;
+        private readonly string _podcastUniqueId;
+
+        public Podcast(string name, string host, int launchYear)
+        {
+            Name = name;
+            Host = host;
+            LaunchYear = launchYear;
+
+            _podcastUniqueId = Guid.NewGuid().ToString();
+        }
+
+        public string GetPodcastInfo() => $"{Name} hosted by {Host}, released in {LaunchYear}";
+
+        public bool IsLaunchedBefore2010(int launchYear) => launchYear < 2010;
+
+        public string UniqueId => _podcastUniqueId;
+    }
+}
---
+++ Session1/PodcastTests.cs
@@ -0,0 +1,76 @@
+﻿using CourseClassLibrary.Domain.Podcasts;
+using Xunit.Abstractions;
+
+namespace Session1
+{
+    public class PodcastTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PodcastTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Setup for test started.");
+        }
+
+        [Fact]
+        public void CanCreatePodcast_WithRequiredFields()
+        {
+            var podcast = new Podcast("Cine-i janghina", "ReallyRux", 2020);
+
+            _output.WriteLine($"Podcast name => {podcast.Name}");
+            _output.WriteLine($"Podcast host => {podcast.Host}");
+            _output.WriteLine($"Podcast launch year => {podcast.LaunchYear}");
+
+            Assert.Equal("Cine-i janghina", podcast.Name);
+            Assert.Equal("ReallyRux", podcast.Host);
+            Assert.Equal(2020, podcast.LaunchYear);
+        }
+
+        [Fact]
+        public void GetPodcastInfo_ShouldContainFormattedDetails()
+        {
+            var podcast = new Podcast("Cine-i janghina", "ReallyRux", 2020);
+
+            _output.WriteLine($"Podcast info => {podcast.GetPodcastInfo()}");
+
+            var podcastInfo = podcast.GetPodcastInfo();
+            Assert.True(podcastInfo.Contains(podcast.Name) &&
+                        podcastInfo.Contains(podcast.Host) &&
+                        podcastInfo.Contains(podcast.LaunchYear.ToString()));
+        }
+
+        [Fact]
+        public void IsClassic_ShouldReturnTrue_ForOldPodcasts()
+        {
+            var podcast = new Podcast("Fierti pe insula", "Sold out media", 2008);
+
+            Assert.True(podcast.IsLaunchedBefore2010(podcast.LaunchYear));
+        }
+
+        [Fact]
+        public void UniquePodcastId_IsAssignedCorrectly()
+        {
+            var podcast1 = new Podcast("Cine-i janghina", "ReallyRux", 2020);
+            var podcast2 = new Podcast("Cine-i janghina", "ReallyRux", 2020);
+
+            _output.WriteLine($"Podcast1 id => {podcast1.UniqueId}");
+            _output.WriteLine($"Podcast2 id => {podcast2.UniqueId}");
+            
+            Assert.NotEqual(podcast1.UniqueId, podcast2.UniqueId);
+        }
+
+        [Fact]
+        public void Podcast_ObjectReference_ShowsSharedState()
+        {
+            var podcast1 = new Podcast("Cine-i janghina", "ReallyRux", 2020);
+            var podcast2 = podcast1;
+            podcast2.Name = "Cine-i janghina 2";
+
+            _output.WriteLine($"Podcast1 name => {podcast1.Name}");
+            _output.WriteLine($"Podcast2 name => {podcast2.Name}");
+
+            Assert.Equal(podcast1.Name, podcast2.Name);
+        }
+    }
+}
