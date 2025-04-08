acf1f61e844bb6ba8b0a2bfc061c6ae17649bd42
ZahDaniel <daniel.zaharia@centric.eu>
2025-04-08 19:43:02 +0300

    Revert "Revert "Session 3""

---
+++ DIP/CookieTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+using Xunit;
+
+public class CookieTest
+{
+    private readonly CookiePopup _cookiePopup;
+
+    public CookieTest(IPage page)
+    {
+        _cookiePopup = new CookiePopup(page); 
+    }
+
+    [Fact]
+    public async Task TestAcceptCookies()
+    {
+        await _cookiePopup.AcceptCookiesAsync();
+    }
+}
