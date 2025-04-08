e35a00ad52a91edf9e4503e4df3775a07b8b6ba0
Andrada Bogdan <andrada.bogdan@centric.eu>
2025-04-08 19:53:36 +0300

    session 3

--- PaymentProcessorTests/PaymentProcessorUnitTests.cs
+++ PaymentProcessorTests/PaymentProcessorUnitTests.cs
@@ -2,43 +2,39 @@ namespace PaymentProcessorTests
 {
     public class PaymentProcessorUnitTests
     {
-        public class PaymentProcessorTests
+        private readonly PaymentProcessor _paymentProcessor;
+
+        public PaymentProcessorUnitTests()
         {
-            private readonly PaymentProcessor _paymentProcessor;
+            _paymentProcessor = new PaymentProcessor();
+        }
 
-            public PaymentProcessorTests()
-            {
-                _paymentProcessor = new PaymentProcessor();
-            }
+        [Fact]
+        public void ProcessPayment_CreditCard_ShouldReturnCorrectMessage()
+        {
+            // Arrange
+            var paymentAmount = 100.00;
+            var paymentMethod = "CreditCard";
 
-            [Fact]
-            public void ProcessPayment_CreditCard_ShouldReturnCorrectMessage()
-            {
-                // Arrange
-                var paymentAmount = 100.00;
-                var paymentMethod = "CreditCard";
+            // Act
+            var result = _paymentProcessor.ProcessPayment(paymentMethod, paymentAmount);
 
-                // Act
-                var result = _paymentProcessor.ProcessPayment(paymentMethod, paymentAmount);
+            // Assert
+            Assert.Equal($"Processing Credit Card payment of ${paymentAmount}...", result);
+        }
 
-                // Assert
-                Assert.Equal($"Processing Credit Card payment of ${paymentAmount}...", result);
-            }
+        [Fact]
+        public void ProcessPayment_PayPal_ShouldReturnCorrectMessage()
+        {
+            // Arrange
+            var paymentAmount = 200.00;
+            var paymentMethod = "PayPal";
 
-            [Fact]
-            public void ProcessPayment_PayPal_ShouldReturnCorrectMessage()
-            {
-                // Arrange
-                var paymentAmount = 200.00;
-                var paymentMethod = "PayPal";
+            // Act
+            var result = _paymentProcessor.ProcessPayment(paymentMethod, paymentAmount);
 
-                // Act
-                var result = _paymentProcessor.ProcessPayment(paymentMethod, paymentAmount);
-
-                // Assert
-                Assert.Equal($"Processing PayPal payment of ${paymentAmount}...", result);
-            }
-
+            // Assert
+            Assert.Equal($"Processing PayPal payment of ${paymentAmount}...", result);
         }
     }
 }
