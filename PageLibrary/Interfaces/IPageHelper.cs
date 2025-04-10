5e556dabac02d2b728cac6e39ee856b0391b55eb
Andrada Bogdan <andrada.bogdan@centric.eu>
2025-04-10 10:06:58 +0300

    solid

---
+++ PageLibrary/Interfaces/IPageHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace PageLibrary.Interfaces
+{
+    public interface IPageHelper
+    {
+        void Click(string selector);
+        void Fill(string selector, string text);
+        void UploadFile(string selector, string filePath);
+    }
+}
